d2733fef5f9cc9e35866b13c2c413da6dc833305
gss-patricia <gss.patricia@gmail.com>
2024-10-06 15:30:15 +0100

    video 1.4

---
+++ src/components/CartEmptyMessage/CartEmptyMessage.tsx
@@ -0,0 +1,9 @@
+import Typography from "../Typography/Typography";
+
+const CartEmptyMessage = () => (
+  <div style={{ marginTop: "15px" }}>
+    <Typography>Não existem produtos no carrinho.</Typography>
+  </div>
+);
+
+export default CartEmptyMessage;
