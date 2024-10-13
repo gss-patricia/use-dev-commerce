fe66dbb6042a18e993461a192a22cbe26f7a4fa3
gss-patricia <gss.patricia@gmail.com>
2024-10-13 12:07:23 +0100

    ajuste nome do diretorio

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import FooterConfig from "./components/Footer/FooterConfig";
-import HomePage from "./Pages/HomePage/HomePage";
-import ProductDetailsPage from "./Pages/ProductDetailsPage/ProductDetailsPage";
+import HomePage from "./pages/HomePage/HomePage";
+import ProductDetailsPage from "./pages/ProductDetailsPage/ProductDetailsPage";
 import ScrollToTop from "./common/utils/ScrollToTop";
 import { CartProvider } from "./common/context/CartContext";
-import CartPage from "./Pages/CartPage/CartPage";
+import CartPage from "./pages/CartPage/CartPage";
 
 function App() {
   return (
